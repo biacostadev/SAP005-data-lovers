6be6d66a7b97f7d5d969b04aef25b8a80d4ce941
Ana Beatriz Costa de Oliveira <anabeatrizcosta.o@gmail.com>
2020-11-25 19:52:25 -0300

    docs: atualizações dos cards e dos filtros

--- data.js
+++ data.js
@@ -1,4 +1,8 @@
-export const selectType = (listaPokemons, filtroType) => (listaPokemons.filter(search => search.type.includes(filtroType)));
+export const selectType = (listaPokemons, filtroType) =>
+    (listaPokemons.filter(search => search.type.includes(filtroType)));
+
+export const selectEgg = (listaPokemons, filtroEgg) =>
+    (listaPokemons.filter(search => search.egg.includes(filtroEgg)));
 
 export const calcType = (data, filtroType) => {
     const type = data.filter(search => search.type.includes(filtroType));
@@ -10,3 +14,21 @@ export const buscarNome = (data, name) =>
 
 export const buscarNum = (data, num) =>
     (data.filter(search => search.num.includes(num)));
+
+const ordenarAZ = (a, b) => (a["name"]).localeCompare(b["name"]);
+const ordenarZA = (a, b) => (a["name"]).localeCompare(b["name"]);
+const ordenarCrescente = (a, b) => Number((a["num"]).localeCompare(b["num"]));
+const ordenarDecrescente = (a, b) => Number((a["num"]).localeCompare(b["num"]));
+
+export const ordenarPokemons = (listaPokemons, filtroOrdenar) => {
+    switch (filtroOrdenar) {
+        case "ordenar-az":
+            return listaPokemons.sort((a, b) => ordenarAZ(a, b))
+        case "ordenar-za":
+            return listaPokemons.sort((a, b) => ordenarZA(a, b)).reverse()
+        case "ordenarCrescente":
+            return listaPokemons.sort((a, b) => ordenarCrescente(a, b))
+        case "ordenarDecescente":
+            return listaPokemons.sort((a, b) => ordenarDecrescente(a, b)).reverse();
+    }
+};
